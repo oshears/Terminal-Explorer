c1160164826ad9091a72abda2b76e35bc5e2e449
Osaze Shears <yoze15@gmail.com>
2014-11-28 14:12:25 -0500

    Hunger Games Scenario Updates

--- HungerGamesTerminalExplorer.py
+++ HungerGamesTerminalExplorer.py
@@ -8,16 +8,19 @@ def main():
 
 	contestants=[NPC(x=0,y=0),NPC(x=0,y=4),NPC(x=0,y=8),NPC(x=0,y=12),NPC(x=0,y=16),NPC(x=0,y=20),
 				NPC(x=4,y=0),NPC(x=8,y=0),NPC(x=12,y=0),NPC(x=16,y=0),NPC(x=20,y=0)]
-	map=Map(contestants,xscale=1,yscale=1,max_row=40, max_col=40)
+
+	centerbox=SqrObjectification(14,20,14,20,color="green")
+
+	map=Map(contestants,centerbox,xscale=1,yscale=1,max_row=21, max_col=21)
 
 	while(True):
 
 		for person in contestants:
-			person.move(map.bounds,contestants)
+			person.move(map.bounds,centerbox,contestants)
 
 
 
-		map.update(contestants)
+		map.update(centerbox,contestants)
 		map.display()
 
 		for person in contestants:
