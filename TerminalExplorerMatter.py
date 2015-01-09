7530a380414c9abab7e68cd906bed26af5ae63f3
Osaze Shears <yoze15@gmail.com>
2015-01-09 11:11:15 -0500

    Programmable Matter Scenario File Update
    Scenario can now read from a cvs file for points

--- TerminalExplorerMatter.py
+++ TerminalExplorerMatter.py
@@ -1,29 +1,22 @@
 from TerminalExplorerAssets import (Player,Map,NPC,Object,Point)
 from TerminalExplorerObjectGen import SqrObjectification,StarObjectification
+import csv
 
 
 def main():
 	userInput=""
+	points = []
 
+	with open("model.csv") as csvfile:
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			#print (row["x"],row["y"])
+			points.append((int(row["x"]),int(row["y"])))
 
-	units = [NPC(x=0,y=0,behavior="Track",track=Point(11,11)),
-		NPC(x=0,y=0,behavior="Track",track=Point(11,12)),
-		NPC(x=0,y=0,behavior="Track",track=Point(11,13)),
-		NPC(x=0,y=0,behavior="Track",track=Point(12,12)),
-		NPC(x=0,y=0,behavior="Track",track=Point(13,12)),
-		NPC(x=0,y=0,behavior="Track",track=Point(14,12)),
-		NPC(x=0,y=0,behavior="Track",track=Point(15,12)),
-		NPC(x=0,y=0,behavior="Track",track=Point(16,12)),
-		NPC(x=0,y=0,behavior="Track",track=Point(17,12)),
-		NPC(x=0,y=0,behavior="Track",track=Point(17,11)),
-		NPC(x=0,y=0,behavior="Track",track=Point(17,10)),
-		NPC(x=0,y=0,behavior="Track",track=Point(17,9)),
-		NPC(x=0,y=0,behavior="Track",track=Point(17,8)),
-		NPC(x=0,y=0,behavior="Track",track=Point(17,7)),
-		NPC(x=0,y=0,behavior="Track",track=Point(17,6)),
-		NPC(x=0,y=0,behavior="Track",track=Point(17,5)),
-		NPC(x=0,y=0,behavior="Track",track=Point(17,4))]
+	units=[]
 
+	for element in points:
+		units.append(NPC(x=0,y=0,behavior="Track",track=Point(element[0],element[1])))
 
 	map=Map(units,xscale=1,yscale=1,max_row=20, max_col=20)
 
